bd084f905e6f9b232e83dfee0fe16c59cb474c80
Alexey Pribelskiy <aeronltd@gmail.com>
2023-09-10 14:25:28 +0600

    models added

--- books/admin.py
+++ books/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from .models import Author, Book, Review
 
-# Register your models here.
+
+class AuthorAdmin(admin.ModelAdmin):
+	list_filter = ('id',)
+
+
+class ReviewAdmin(admin.ModelAdmin):
+	list_display = ('id', 'rating', 'review_date', 'content', 'user')
+	list_display_links = ('id', 'rating')
+	search_fields = ('id', 'rating', 'review_date')
+	list_filter = ('rating', 'review_date')
+
+class BookAdmin(admin.ModelAdmin):
+	list_display = ('id', 'title', 'publish_date', 'description')
+	list_display_links = ('title',)
+	search_fields = ('id', 'publish_date')
+	list_filter = ('publish_date',)
+
+
+admin.site.register(Author, AuthorAdmin)
+admin.site.register(Book, BookAdmin)
+admin.site.register(Review, ReviewAdmin)
--- books/models.py
+++ books/models.py
@@ -1,3 +1,46 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Author(models.Model):
+	firstname = models.CharField(max_length=50, verbose_name='Имя')
+	lastname = models.CharField(max_length=100, verbose_name='Фамилия')
+	biography = models.TextField(blank=True, verbose_name='Биография')
+
+	class Meta:
+		verbose_name = 'Автор'
+		verbose_name_plural = 'Авторы'
+
+	def __str__(self):
+		return f"{self.firstname} {self.lastname}"
+
+
+class Book(models.Model):
+	title = models.CharField(max_length=200, verbose_name='Название')
+	description = models.TextField(blank=True, verbose_name='Описание')
+	publish_date = models.IntegerField(verbose_name='Дата публикации')
+	book_author = models.ForeignKey(Author, on_delete=models.CASCADE)
+
+	class Meta:
+		verbose_name = 'Книга'
+		verbose_name_plural = 'Книги'
+		ordering = ['-publish_date']
+
+	def __str__(self):
+		return f"{self.title}"
+
+
+class Review(models.Model):
+	content = models.TextField(max_length=500, verbose_name='Отзыв')
+	rating = models.IntegerField(verbose_name='Рейтинг')
+	review_date = models.DateTimeField(auto_now=True, verbose_name='Дата публикации')
+	user = models.ForeignKey(User, verbose_name='Пользователь', on_delete=models.CASCADE)
+	book = models.ForeignKey(Book, verbose_name='Книга', on_delete=models.PROTECT)
+
+	class Meta:
+		verbose_name = 'Описание'
+		verbose_name_plural = 'Описание'
+		ordering = ['rating']
+
+	def __str__(self):
+		return f"{self.content} {self.rating}"
